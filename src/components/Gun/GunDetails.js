1618ce0d1fa223d5e4e5a39a4bd268df473ac4a1
Rudy-42 <64607644+Rudy-42@users.noreply.github.com>
2021-11-14 23:00:52 +0200

    notifications

--- src/components/Gun/GunDetails.js
+++ src/components/Gun/GunDetails.js
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
 
 export function GunDetails() {
     const { id } = useParams();
@@ -11,12 +13,24 @@ export function GunDetails() {
             'Content-type': 'application/json',
         },
         })
-        .then((res) => {return res.json();})
-        .then((data) => {setGun(data[0])});
+        .then((res) => {
+          if (res.ok){
+            NotificationManager.success('Gun loaded', 'SUCCESS');
+            return res.json();
+          }else{
+            throw new Error(res.status)
+          }
+         
+        })
+        .then((data) => {setGun(data[0])})
+        .catch((error) => {
+          NotificationManager.error('API call failed with status code:"'+error+'" !', 'ERROR', 5000);
+        });;
         
     },[id])
 
     return (
+    <>
        <div>
         <p>{id}</p>
           <img src={gun?.image} alt="Gun no img"></img>
@@ -24,5 +38,7 @@ export function GunDetails() {
           <p>{gun?.type}</p>
           <p>{gun?.caliber}</p>
         </div>
+        <NotificationContainer/>
+      </>
     );
   }
