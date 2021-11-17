6bf41be5e0ed2fc0ccac8b54b58dc9084d4365c5
unknown <sebanistor@gmail.com>
2021-11-17 20:33:36 +0200

    reversed changes and more changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,10 +4,9 @@ export function Home() {
   return (
     <>
     <div className="wholepage">
-      <h1 className="title">Welcome to Guns for Hire</h1>
       <div className="grid-container">        
-        <p className="ptest" >Join private militias around the world, fight alongside and against grizzled veterans in your quest for money and glory. Partake in revolts and "holy" wars. Gather enough accomplishments and you may even leave your mark in the history books.</p>
-        <img className="image" src="https://wallpapercave.com/wp/wp3372267.jpg" alt="war" />
+        <img className="image topimg" src="https://wallpapercave.com/wp/wp3372267.jpg" alt="war" />
+        {/* <p className="banner" >Join private militias around the world, fight alongside and against grizzled veterans in your quest for money and glory. Partake in revolts and "holy" wars. Gather enough accomplishments and you may even leave your mark in the history books.</p> */}
         <img className="image" src="./Images/War/wp9393529.jpg" alt="" srcset="" />
       </div>
     </div>
