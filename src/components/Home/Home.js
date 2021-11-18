e0c71597dc2209138b0e4410bc0912a6a3793373
unknown <sebanistor@gmail.com>
2021-11-18 17:00:49 +0200

    whatever it is it is

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,14 @@ export function Home() {
     <>
     <div className="wholepage">
       <div className="grid-container">        
-        <img className="image topimg" src="https://wallpapercave.com/wp/wp3372267.jpg" alt="war" />
+        {/* <img className="image topimg" src="https://wallpapercave.com/wp/wp3372267.jpg" alt="war" /> */}
         {/* <p className="banner" >Join private militias around the world, fight alongside and against grizzled veterans in your quest for money and glory. Partake in revolts and "holy" wars. Gather enough accomplishments and you may even leave your mark in the history books.</p> */}
-        <img className="image" src="./Images/War/wp9393529.jpg" alt="" srcSet="" />
+        <img className="image" className="poster" src="./Images/War/wp9393529.jpg" alt="" srcSet="" />
+        <div className="infodown">
+          <p>Join many battlefronts worldwide for money and glory</p>
+          <p>Open beta v1.4</p>
+          <button>Play</button>
+        </div>
       </div>
     </div>
     </>
